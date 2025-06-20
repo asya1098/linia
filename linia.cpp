b81e3432906a5ae0aadbc359c11194e372cba389
asya <arsenijmozgov487@gmail.com>
2025-06-20 20:30:59 +0300

    reborn linia. v0.1

--- linia.cpp
+++ linia.cpp
@@ -5,29 +5,26 @@
 int main()
 { 
     srand(time(NULL)); 
-    // Настройка локали
     setlocale(LC_ALL,"ru");
-    // Переменные
     char symbol;
-    int height;
-    int width;
+    int *height = {new int};
+    int *width = {new int};
     std::string random;
-    int limit = 0;
+    int *limit = {new int};
 
-    // Ввод данных
     std::cout << "Linia - это программа которая выводит на экран геометрические фигуры с заданным символом.\n\n"; 
     std::cout << "Какой использовать символ: ";
     std::cin >> symbol;
     error:
     std::cout << "Сделать произвольную фигуру?(высота и ширина будут подобраны случайным образром): ";
     std::cin >> random;
-    if(random == "да"){
+    if(random == "да" || random == "yes"){
         std::cout << "Введите лимит символов: ";
-        std::cin >> limit; 
-        height = 1 + rand() % limit;
-        width = 1 + rand() % limit;
+        std::cin >> *limit; 
+        *height = 1 + rand() % *limit;
+        *width = 1 + rand() % *limit;
         goto linkRandom;
-    }else if(random == "нет"){
+    }else if(random == "нет" || random == "no"){
         std::cout << "Хорошо" << std::endl;
     }else{
         std::cout << "Неправильный Ввод.Введите да или нет" << std::endl; 
@@ -35,14 +32,14 @@ int main()
     }
 
     std::cout << "Введите высоту: ";
-    std::cin >> height;
+    std::cin >> *height;
     std::cout << "Введите ширину: ";
-    std::cin >> width;
+    std::cin >> *width;
 
     linkRandom:
-    for(int i = 0; i < height; i++)
+    for(int i = 1; i < *height; i++)
     {
-        for(int a = 0; a < width; a++){
+        for(int a = 0; a < *width; a++){
             std::cout << symbol;
         }          
         std::cout << symbol << std::endl;
